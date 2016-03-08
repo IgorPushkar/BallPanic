fe81e9427ab156ad7833e2607198a0d77b194427
Igor Pushkar <igor.pushkar@gmail.com>
2016-03-08 21:30:45 +0200

    Created menus and basic gameplay (player animation and shots, balls split-up on being hit, destructible bricks animation) and scripts for it.

---
+++ Assets/Scripts/Ball/BallScript.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	private float forceX, forceY;
+	private bool moveLeft = true;
+	private Rigidbody2D body;
+
+	[SerializeField]
+	private GameObject originalBall;
+
+	private GameObject ball1, ball2;
+
+	private BallScript ball1Script, ball2Script;
+
+	[SerializeField]
+	private AudioClip[] clip;
+
+	void Awake(){
+		body = GetComponent<Rigidbody2D> ();
+		SetBallSpeed ();
+		InstantiateBalls ();
+
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		MoveBall ();
+	}
+
+	void InstantiateBalls(){
+		if(this.gameObject.tag != "SmallestBall"){
+			ball1 = Instantiate (originalBall);
+			ball2 = Instantiate (originalBall);
+
+			ball1Script = ball1.GetComponent<BallScript> ();
+			ball2Script = ball2.GetComponent<BallScript> ();
+
+			ball1.SetActive (false);
+			ball2.SetActive (false);
+		}
+	}
+
+	public void SetMoveLeft(bool flag){
+		moveLeft = flag;
+	}
+
+	void MoveBall(){
+		Vector3 temp = transform.position;
+		temp.x += moveLeft ? -(forceX * Time.deltaTime) : (forceX * Time.deltaTime);
+		transform.position = temp;
+	}
+
+	void InitNewBalls(){
+		Vector3 position = transform.position;
+
+		ball1.transform.position = position;
+		ball1Script.SetMoveLeft(true);
+		ball1.SetActive (true);
+
+		ball2.transform.position = position;
+		ball2Script.SetMoveLeft(false);
+		ball2.SetActive (true);
+
+		if(this.gameObject.tag != "SmallestBall"){
+			if(transform.position.y > 1 && transform.position.y < 1.3f){
+				ball1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 3.5f);
+				ball2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 3.5f);
+			} else if(transform.position.y > 1.3f){
+				ball1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 2f);
+				ball2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 2f);
+			} else if (transform.position.y > 1f){
+				ball1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 5.5f);
+				ball2.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 5.5f);
+			}
+		}
+	}
+
+	void SetBallSpeed(){
+		forceX = 2.5f;
+		switch(gameObject.tag){
+		case "LargestBall":
+			forceY = 11.5f;
+			break;
+		case "LargeBall":
+			forceY = 10.5f;
+			break;
+		case "MediumBall":
+			forceY = 9f;
+			break;
+		case "SmallBall":
+			forceY = 8f;
+			break;
+		case "SmallestBall":
+			forceY = 7f;
+			break;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "FirstArrow" || other.tag == "SecondArrow" || other.tag == "FirstStickyArrow" || other.tag == "SecondStickyArrow"){
+			if(gameObject.tag != "SmallestBall"){
+				InitNewBalls ();
+			}
+			AudioSource.PlayClipAtPoint (clip[Random.Range(0,clip.Length)], transform.position);
+			gameObject.SetActive (false);
+		}
+
+		if(other.tag == "UnbreakableBrickTop" || other.tag == "BreakableBrickTop" || other.tag == "UnbreakableBrickTopVertical"){
+			body.velocity = new Vector2 (0, 5);
+		} else if(other.tag == "UnbreakableBrickBottom" || other.tag == "BreakableBrickBottom" || other.tag == "UnbreakableBrickBottomVertical"){
+			body.velocity = new Vector2 (0, -2);
+		} else if(other.tag == "UnbreakableBrickLeft" || other.tag == "BreakableBrickLeft" || other.tag == "UnbreakableBrickLeftVertical"){
+			moveLeft = false;
+		} else if(other.tag == "UnbreakableBrickRight" || other.tag == "BreakableBrickRight" || other.tag == "UnbreakableBrickRightVertical"){
+			moveLeft = true;
+		}
+
+		if(other.tag == "BottomBrick"){
+			body.velocity = new Vector2 (0, forceY);
+		}
+		if(other.tag == "LeftBrick"){
+			moveLeft = false;
+		}
+		if(other.tag == "RightBrick"){
+			moveLeft = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private float speed = 8.0f;
+	private float maxVelocity = 4.0f;
+
+	private Rigidbody2D body;
+	private Animator anim;
+
+	[SerializeField]
+	private GameObject[] arrows;
+
+	private float height;
+	private bool shootOnce, shootTwice;
+	private bool canWalk;
+
+	[SerializeField]
+	private AudioClip shootClip;
+
+	void Awake(){
+		body = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		height = -Camera.main.orthographicSize - 0.5f;
+		canWalk = true;
+		shootOnce = true;
+		shootTwice = true;
+	}
+
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		MovementKeyboard ();
+	}
+
+	void Update(){
+		ShootArrow ();
+	}
+
+	public void SetShootOnce(){
+		shootOnce = true;
+	}
+
+	public void SetShootTwice(){
+		shootTwice = true;
+	}
+
+	public void ShootArrow(){
+		if(Input.GetMouseButtonDown(0)){
+			if (shootOnce) {
+				shootOnce = false;
+				StartCoroutine (PlayShootAnimation ());
+				Instantiate (arrows [2], new Vector3 (transform.position.x, height, 0), Quaternion.identity);
+			} else if (shootTwice) {
+				shootTwice = false;
+				StartCoroutine (PlayShootAnimation ());
+				Instantiate (arrows [1], new Vector3 (transform.position.x, height, 0), Quaternion.identity);
+			}
+
+		}
+	}
+
+	IEnumerator PlayShootAnimation(){
+		canWalk = false;
+		anim.SetBool ("Shoot", true);
+		string clipName = name + " Shoot";
+		anim.Play (clipName);
+		AudioSource.PlayClipAtPoint (shootClip, transform.position);
+		yield return new WaitForSeconds (0.15f);
+		anim.SetBool ("Shoot", false);
+		canWalk = true;
+	}
+
+	void MovementKeyboard(){
+		if(canWalk){
+			float force = 0.0f;
+			float velocity = Mathf.Abs (body.velocity.x);
+
+			float h = Input.GetAxis ("Horizontal");
+
+			if(h > 0){
+				if(velocity < maxVelocity){
+					force = speed;
+				}
+				Vector3 scale = transform.localScale;
+				scale.x = 1.0f;
+				transform.localScale = scale;
+			} else if (h < 0){
+				if(velocity < maxVelocity){
+					force = -speed;
+				}
+				Vector3 scale = transform.localScale;
+				scale.x = -1.0f;
+				transform.localScale = scale;
+			}
+
+			body.AddForce (new Vector2 (force, 0));
+
+			anim.SetBool ("Walk", velocity > 0);
+		}
+	}
+}
